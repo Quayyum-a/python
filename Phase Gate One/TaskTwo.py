8fc702a134b4d0fb0d9a3d7ee059b9a62568d147
Quayyum-a <quayyumariyo@gmail.com>
2025-01-15 12:09:12 +0100

    commit

--- Phase Gate One/TaskTwo.py
+++ Phase Gate One/TaskTwo.py
@@ -1,6 +1,18 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
+try:
+    num1 = int(input("Enter first number: "))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+   
+try:
+    num2 = int(input("Enter second number: "))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+
+try:
+    num3 = int(input("Enter third number: "))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+    
 
 ascending_numbers = sorted([num1, num2, num3])
 print(ascending_numbers)
